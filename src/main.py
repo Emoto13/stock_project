51055828364e7e6df6663892951c615fe29e718e
Emoto13 <emcho_spassov@abv.bg>
2021-05-17 16:17:54 +0300

    Update example main.py

--- src/main.py
+++ src/main.py
@@ -1,18 +1,33 @@
-from StockPredictionWrapper import StockPredictionWrapper
+from visualization.PlotWriterReader import PlotWriterReader
+from pipeline.data_transformers import PreProcessor, Smoother
+from utils import DateTimeOperator
+from models import LSTMWrapper
+from sklearn.preprocessing import MinMaxScaler
+import pandas as pd
+import pandas_datareader as web
 
-key = "your_key"
-host = "alpha-vantage.p.rapidapi.com"
-function = "TIME_SERIES_WEEKLY"
-symbol = "NFLX"
+key = "key"
+periodicity = "daily"
+symbol = "GOOGL"
+
+df = web.DataReader(symbol, f"av-{periodicity}", api_key=key)
+pred_df = PreProcessor(df).preprocess_alpha_vantage_df()
+original_df = pred_df.copy()
+original_df.set_index(['ds'], inplace=True)
+
+scaler = MinMaxScaler(feature_range=(0,50))
+pred_df.y = scaler.fit_transform(pred_df.y.values.reshape(-1,1))[:,0]
+
+pred_df.y = Smoother.smooth(pred_df.y.values, 'moving_average', 30)
     
-prediction_df, forecast = StockPredictionWrapper()\
-.run_prediction(url='https://alpha-vantage.p.rapidapi.com/query',
-                symbol=symbol,
-                function=function,
-                rapidapi_key=key,
-                rapidapi_host=host,
-                convert_to_stationary=True,
-                stationarity_test='adf',
-                smoother='lowess',
-                save_plot=True)
-    
+save_path = f'reports/stocks/predictions/{symbol}/{DateTimeOperator.get_current_date_and_time()}_lstm_{periodicity}.png'
+lstmw = LSTMWrapper(dataframe=df, periodicity='daily', epochs=35, neurons=10, dropout=0.2, days_ahead=300)
+forecast = lstmw.run()
+
+
+pred_df.rename(columns={"y":"yhat"}, inplace=True)
+result = pred_df.append(forecast)
+result.set_index(['ds'], inplace=True)
+result.yhat = scaler.inverse_transform(result.yhat.values.reshape(-1,1))[:,0]
+
+PlotWriterReader(original_df, result).save_original_and_prediction_plot(save_path=save_path)
